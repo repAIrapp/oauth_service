de870685190735df2ba0cb459802f828e9e6404b
Yasmineibr1 <yasmine.brahimi321@gmail.com>
2025-07-07 00:36:36 +0200

    fix:metrics route

--- index.js
+++ index.js
@@ -3,16 +3,46 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const router = require('./src/routes/index');
+const client = require("prom-client");
 
 require('./src/config/passportConfig');
 
 const app = express();
+const register = new client.Registry();
+// Crée une métrique de type Counter
+const oauthRequestsCounter = new client.Counter({
+  name: "oauth_requests_total",
+  help: "Nombre total de requêtes sur le service OAuth",
+  labelNames: ["method", "route", "status"]
+});
+
+// Enregistre la métrique dans le registre
+register.registerMetric(oauthRequestsCounter);
+
+// Collecte les métriques système par défaut
+client.collectDefaultMetrics({ register });
+
+// Middleware pour enregistrer chaque requête
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    oauthRequestsCounter.inc({
+      method: req.method,
+      route: req.path,
+      status: res.statusCode
+    });
+  });
+  next();
+});
 
 app.use(session({
     secret: process.env.SESSION_SECRET || 'secret',
     resave: false,
     saveUninitialized: true
 }));
+app.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", register.contentType);
+  res.send(await register.metrics());
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -31,4 +61,14 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
+const metricsApp = express();
+// metricsApp.use(express.json()); 
+// metricsApp.use(require("cors")());
+metricsApp.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", register.contentType);
+  res.send(await register.metrics());
+});
+metricsApp.listen(9104, () => {
+  console.log("📊 oauth service metrics exposed on http://localhost:9104/metrics");
 });
